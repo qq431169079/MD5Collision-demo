4bfb8892bcf7d0737ddbada3f6673e533737fb6d
Wessel Meijer <wesselmjr@gmail.com>
2018-11-13 13:58:55 +0000

    Fixed lack of quotations

--- installer.py
+++ installer.py
@@ -1,6 +1,6 @@
 import urllib.request
 
-input("Installing file 'good' from 'https://iksman.com/projects/md5' >>)
+input("Installing file 'good' from 'https://iksman.com/projects/md5' >>")
 urllib.request.urlretrieve("https://iksman.com/projects/md5/good" , "good")
 print("Succesfully installed file 'good' to folder")
 
--- installer_evil.py
+++ installer_evil.py
@@ -1,6 +1,6 @@
 import urllib.request
 
-input("Installing file 'evil' from 'https://iksman.com/projects/md5' >>)
+input("Installing file 'evil' from 'https://iksman.com/projects/md5' >>")
 urllib.request.urlretrieve("https://iksman.com/projects/md5/evil" , "evil")
 print("Succesfully installed file 'evil' to folder")
 
